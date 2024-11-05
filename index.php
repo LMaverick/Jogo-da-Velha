97284b72731ffdc413f7285e99ec2ac88b719f14
LMaverick <91562063+LMaverick@users.noreply.github.com>
2024-11-05 23:26:10 +0000

    up

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Jogo da velha</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container">
+    <h1>Jogo da Velha</h1>
+    <div id="tabuleiro">
+      <div class="linha">0
+        <button class="casa" data-pos="0"></button>
+        <button class="casa" data-pos="1"></button>
+        <button class="casa" data-pos="2"></button>
+      </div>
+      <div class="linha">
+        <button class="casa" data-pos="3"></button>
+        <button class="casa" data-pos="4"></button>
+        <button class="casa" data-pos="5"></button>
+      </div>
+      <div class="linha">
+        <button class="casa" data-pos="6"></button>
+        <button class="casa" data-pos="7"></button>
+        <button class="casa" data-pos="8" ></button>
+      </div>
+    </div>
+    <button id="reiniciar">Reiniciar Jogo</button>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+</html>
