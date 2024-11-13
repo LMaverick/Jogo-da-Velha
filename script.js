3e8a4e0fac05167196d5d56f9d416fb0d9ff6f3b
LMaverick <91562063+LMaverick@users.noreply.github.com>
2024-11-13 00:02:03 +0000

    up

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 let tabuleiro = ['','','','','','','','',''];
-let definePlayer = "1";
 
 let ganhou = [[0,1,2],[3,4,5],[6,7,8],
               [0,3,6],[1,4,7],[2,5,8],
@@ -15,4 +14,17 @@ let vez = 'X';
             tabuleiro[index] = vez;
             document.querySelectorAll('.casa')[index].textContent = vez;
             vez = (vez === 'X') ? 'O' : 'X';
+            VerificarGanhador(tabuleiro,vez);
+
+
+        }
+
+        function VerificarGanhador(tabuleiro,jogada){
+
+            for(let i = 0; i< ganhou.length;i++){
+                const combinacao = ganhou[i];
+                if (tabuleiro[ganhou[0]] != '' && tabuleiro[ganhou[0]] == tabuleiro[ganhou[1]] == tabuleiro[ganhou[2]])
+            alert('Ganhou');
+            
+                }
         }
